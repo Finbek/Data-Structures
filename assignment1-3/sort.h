722789f0a24c907ee033a242e319c6e72d628916
Dilshod Bakhtiyorov <dilshod@unist.ac.kr>
2020-04-02 10:54:12 +0000

    Update sort.h

--- assignment1-3/sort.h
+++ assignment1-3/sort.h
@@ -1,5 +1,40 @@
-void sort(int* numbers, int N) {
-	// implement your sorting algorithm
+void swap(int *num1, int *num2);
+void quicksort(int *numbers, int First, int Last);
+void sort(int* numbers)
+{   
+    int N=sizeof(numbers)-1;
+    quicksort (numbers, 0, N);
 }
 
-// Add functions if you need to
+
+
+void quicksort(int* numbers, int First, int Last)
+{
+    if(First>=Last)
+    {
+        return;
+    }
+    // implement your sorting algorithm
+    int partion=numbers[Last];
+    int pivotStartIndex=First;
+
+    for(int k=First; k<=Last; k++)
+    {
+        if(partion>=numbers[k])
+        {
+            swap(&numbers[pivotStartIndex], &numbers[k]);
+            pivotStartIndex++;
+        }
+    }
+    quicksort(numbers, First, pivotStartIndex-2);
+    quicksort(numbers, pivotStartIndex, Last);
+}
+
+
+void swap(int *num1, int *num2)
+{
+    int copy=*num1;
+    *num1=*num2;
+    *num2=copy;
+}
+                                                              13,5          Top

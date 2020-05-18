fdff97f9c331538b5df3d83af6ee508b2d3e930c
Dishod <Dilshod@unist.ac.kr>
2020-05-18 21:11:41 +0900

    Dilshod Bakhtiyorov

--- assignment4/main.cpp
+++ assignment4/main.cpp
@@ -1,6 +1,6 @@
-#include "min_heap.h"
-#include "lfu.h"
+
 #include <iostream>
+#include "min_heap.h"
 
 void print(bool result) {
 	if (result) {
@@ -16,12 +16,12 @@ int main(void) {
 	int myints[] = { 10, 20, 30, 5, 60 };
 
 	MinHeap<int>::make_heap(myints, 5);
-	std::cout << "initial top	:" << MinHeap<int>::top(myints) << std::endl;
+	std::cout << "initial top	:" << myints[0] << std::endl;
 	MinHeap<int>::pop(myints, 5);
-	std::cout << "pop	:" << MinHeap<int>::top(myints) << std::endl;
+	std::cout << "pop	:" << myints[0] << std::endl;
 	MinHeap<int>::push(myints, 4, 2);
-	std::cout << "push	:" << MinHeap<int>::top(myints) << std::endl;
-
+	std::cout << "push	:" << myints[0] << std::endl;
+/*
 	// Part2
 	LFU<int> lfu(3);
 
@@ -31,6 +31,7 @@ int main(void) {
 	print(lfu.touch(1)); // hit
 	print(lfu.touch(2)); // hit
 	print(lfu.touch(4)); // miss
-
+*/
 	return 0;
+
 }
--- assignment4/min_heap.h
+++ assignment4/min_heap.h
@@ -7,14 +7,83 @@ struct MinHeap {
 
 	static void make_heap(T* arr, int size, int hint = -1) {
 		// Build heap
+		if(hint==-1)
+		{
+			//heapify
+			for(int i = size/2-1; i>=0; i--){
+				PerlocateDown(arr, size, i);
+			} 
+		}
+		else{
+			//Check whether perlocateUp or PerlocateDown
+			int par = (hint-1)/2;
+			if(arr[hint]>arr[2*hint+1] || arr[hint]>arr[hint*2+2])
+			{
+				PrelocateDown(arr, size, hint);	
+			}
+			else if(arr[par]>arr[hint]){
+				PerlocateUp(arr, hint);
+			}
+		}
 	}
 
 	static void pop(T* arr, int size) {
 		// Delete minimum
+		arr[0]=arr[size-1];
+		PerlocateDown(arr, size-1, 0);
+		
 	}
 
 	static void push(T*	arr, int size, T item) {
 		// Push item
+		arr[size]=item;
+		PerlocateUp(size);
 	}
-};
+	static void PerlocateDown(T*arr, int size, int index)
+	{
+		//This function perlocate down the given heap by the provided index, additional function
+		//left child index
+		int Child=2*index+1;
+		T* value = arr[index];
+		while(Child<size){
+			T minValue = value;
+			int minIndex = -1;
+			for( int i=0; i<2 && i+Child<size; i++)
+			{
+				if(arr[i+Child]<minValue)
+					{
+						minValue=arr[i+Child];
+						minIndex=i+Child;
+					}
+			}	
+			if(minValue==value)
+				return false;//the given index has a minimum range
+			else {
+				swap(arr[index], arr[minIndex]);
+				index = minIndex;
+				Child=2*index+1;			
+			}
+		}
+}  
+	static void PerlocateUp(T* arr, int index)
+	{
+		while(index>0){
+			//Index of parent
+			int parent = (index-1)/2;
+			if(arr[index]>=arr[parent])
+				return false;
+			else {
+				swap(arr[index], arr[parent]);
+				index = parent;
+			}
+		}
+	}
+	//Swap array items
+	static void swap(T* arr1, T* arr2)
+	{
+		T temp= *arr1;
+		*arr1 = *arr2;
+		*arr2=temp;
+
+	}
 #endif

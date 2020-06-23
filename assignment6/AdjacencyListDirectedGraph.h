ed1d8e8a247db286a3aa22342c26ac9a6cc118f9
Dishod <Dilshod@unist.ac.kr>
2020-06-23 23:34:01 +0900

    Dilshod

--- assignment6/AdjacencyListDirectedGraph.h
+++ assignment6/AdjacencyListDirectedGraph.h
@@ -104,14 +104,14 @@ public:
      * Return the element stored at this vertex.
      */
     V& operator*() const {
-
+	return v_obj->elt; //Just return the stored element.
     }
 
     /*
      * Return a list of edges incident to this vertex.
      */
     EdgeList incidentEdges() const {
-
+	return *(v_obj->inc_edges_pos);//Easy return of the stored incident edges list 
     }
 
     /*
@@ -122,19 +122,47 @@ public:
      * v - the given vertex
      */
     bool isAdjacentTo(const Vertex& v) const {
+	EdgeList& edgeListObj = *v_obj->inc_edges_pos;
+	EdgeList& edgeListV = *v.v_obj->inc_edges_pos;
+	int counter;//We will compare edges, to avoid seg. fault, we will take counter as least of the list size
+	EdgeList comp;//Edges to compare, the least edge list among two
+	if(edgeListObj.size()>edgeListV.size())
+	{
+		counter=edgeListV.size();
+		comp=edgeListV;
+	}
+	else
+	{
+		counter=edgeListObj.size();
+		comp=edgeListObj;
+	}
+	for(EdgeItor i = comp.begin(); i != comp.end(); i++)//Among the comp edges, if we find an edge satisfying the conintion, we return true, otherwise it will give false at the end of the function
+	{
+		if(((*i).origin()==(*this) && (*i).dest()==v) || (((*i).origin()==v && (*i).dest()==(*this))))
+			{return true;
+			}
+	}
+	return false;
 
-
-    }
+    
+}
 
     /*
-     * Check whether there is a directed edge connecting this vertex to the given vertex.
+     * C 	heck whether there is a directed edge connecting this vertex to the given vertex.
      *
      * v - the given vertex
      */
     bool isOutgoingTo(const Vertex& v) const {
-
+	//This check is alomost the same as the isAdjacentTo, but now we need strictly v-destination, this-origin
+	EdgeList& edges = *(v_obj->inc_edges_pos);
+	for(EdgeItor i = edges.begin(); i != edges.end(); i++){//Consider all edges
+		{
+			if(((*i).origin()==(*this)) && ((*i).dest()==v))//Described above strict condition
+				return true;
+		}
+	return false;//Otherwise
     }
-
+}
     /*
      * Return a directed edge connecting this vertex to the given vertex.
      * If the directed edge does not exist, throw an exception.
@@ -143,16 +171,33 @@ public:
      * Return the directed edge connecting this vertex to the given vertex.
      */
     Edge outgoingEdge(const Vertex& v) const {
-
+	//We will check the edges of this vertex as we did in bool isOutgoingTo and now retrun satisfying;
+	EdgeList& edges = *v_obj->inc_edges_pos;
+        for(EdgeItor i = edges.begin(); i != edges.end(); i++)//Consider all edges
+                {
+                        if((*i).origin()==(*this) && (*i).dest()==v)//Described above strict condition
+                                return *i;
+                }
+        throw runtime_error("NO SUCH EDGE");//Otherwise
     }
 
     /*
      * Return the set of all directed edges connecting this vertex to any vertex.
      */
+
+
     EdgeList outgoingEdges() const {
+	//Same as the outgoingEdge, but we will create a list and push_back new edges there;
+	EdgeList outGoing;
+	EdgeList& edges = *v_obj->inc_edges_pos;
 
+        for(EdgeItor i = edges.begin(); i != edges.end(); i++)//Consider all edges
+                {
+                        if((*i).origin()==*(this))//Described above strict condition
+                                outGoing.push_back(*i);
+                }
+	return outGoing;
     }
-
     /*
      * Check whether this vertex is the same as the given vertex
      *
@@ -160,7 +205,10 @@ public:
      * Return true if this vertex is the same as the given vertex
      */
     bool operator==(const Vertex& v) const {
-
+	if(v.v_obj==v_obj)//If the v_obj of both vertices is the same, they are equal
+		return true;
+	else 
+		return false;
     }
 
     /*
@@ -192,7 +240,7 @@ public:
      * Return the element stored at this edge.
      */
     E& operator*() const {
-
+	return e_obj->elt;//Return stored in e_obj element
     }
 
     /*
@@ -201,7 +249,10 @@ public:
      * The second element of the vertex list is the vertex of the destination.
      */
     VertexList endVertices() const {
-
+	VertexList endV;//Create empty list
+	endV.push_back(e_obj->origin_vertex);//First push origin
+	endV.push_back(e_obj->dest_vertex);//Then destination
+	return endV;
     }
 
     /*
@@ -214,7 +265,12 @@ public:
      * Return the other vertex of this edge
      */
     Vertex opposite(const Vertex& v) const {
-
+	if(v==e_obj->origin_vertex)
+		return e_obj->dest_vertex;//If v is origin, then return destination
+	else if(v==e_obj->dest_vertex)
+		return e_obj->origin_vertex;//if v is destination, return origin
+	else 
+		throw runtime_error("NO SUCH VERTEX IN THE EDGE");//Exception
     }
 
     /*
@@ -226,7 +282,8 @@ public:
      * Return true if the given edge is adjacent to this edge.
      */
     bool isAdjacentTo(const Edge& edge) const {
-
+	//We can check two vertices of the edge, is one of  them incident to our edge
+	return ((edge.isIncidentOn(e_obj->origin_vertex)) || (edge.isIncidentOn(e_obj->dest_vertex))); 
     }
 
     /*
@@ -237,21 +294,24 @@ public:
      * Return true if the given vertex is incident to this edge.
      */
     bool isIncidentOn(const Vertex& v) const {
-
+	if(e_obj->origin_vertex==v || e_obj->dest_vertex==v)//Direct check of the two options if they are equal to the v
+		return true;
+	else
+		return false;
     }
 
     /*
      * Return the vertex at the origin of this edge.
      */
     Vertex origin() const {
-
+	return e_obj->origin_vertex;//return origin vertex
     }
 
     /*
      * Return the vertex at the destination of this edge.
      */
     Vertex dest() const {
-
+	return e_obj->dest_vertex;//return destination vertex
     }
 
     /*
@@ -259,7 +319,7 @@ public:
      * In this class, it should always return true.
      */
     bool isDirected() const {
-
+	return true;//alsways true
     }
 
     /*
@@ -269,7 +329,7 @@ public:
      * Return true if this edge is the same as the given edge.
      */
     bool operator==(const Edge& edge) const {
-
+	return edge.e_obj==e_obj;//compare e_obj of two
     }
 
     /*
@@ -286,15 +346,31 @@ public:
   /*
    * Return the list of vertices in this graph.
    */
+
   VertexList vertices() {
-
-  }
-
+	//they are almost the same as vertex_collection, but in vertex_collection we have VertexObject, we need to make them Vertices;
+	VertexList answer;
+	for(VertexObjectItor i = vertex_collection.begin(); i != vertex_collection.end(); i++)
+	{
+		Vertex v;
+		v.v_obj=&(*i);
+		answer.push_back(v);	//Here we will make vertex each VertexObject and add to answer
+	}
+	return answer; 
+  
+}
   /*
    * Return the list of edges in this graph.
    */
   EdgeList edges() {
-
+	//Same as VertexList, but with edges
+	EdgeList answer;
+	for(EdgeObjectItor i = edge_collection.begin(); i != edge_collection.end();i++){
+		Edge ed;
+		ed.e_obj=&(*i);
+		answer.push_back(ed);
+	}
+	return answer;
   }
 
   /*
@@ -304,7 +380,20 @@ public:
    * Return the newly created vertex.
    */
   Vertex insertVertex(const V& x) {
-
+	//Fist we need to create VertexObject for the new element;
+	VertexObject verObj = VertexObject(x);
+	vertex_collection.push_back(verObj);//Adding to the collection
+	VertexObjectItor i  = vertex_collection.end();
+	i--;//the pos of the new VertexObject
+	VertexObject& newV=(*i);
+	newV.pos=i;//Correcting pos;
+	inc_edges_collection.push_back(EdgeList());//Adding to the collection, NULL 
+	IncidenceEdgesItor ie = inc_edges_collection.end();
+	ie--;//Position in the inc_edges_collection
+	newV.inc_edges_pos=ie;//correcting pos
+	Vertex v; //Creation of vertex
+	v.v_obj=&newV;
+	return v;// returning vertex
   }
 
   /*
@@ -316,10 +405,33 @@ public:
    * x - the element to be stored in the new edge.
    * Return the newly created edge.
    */
+
   Edge insertDirectedEdge(const Vertex& v, const Vertex& w, E x) {
+	if(!v.isOutgoingTo(w))//Checking existance, if there is no, will do almost the same as insertVertex(), no need to explain similar analogue 
+	{	EdgeObject newEdOb = EdgeObject(v, w, x);//creating new edge_object
+		edge_collection.push_back(newEdOb);// adding to the edge_collect
+		EdgeObjectItor i= edge_collection.end();
+		i--;//pos of the edj_object
+		EdgeObject &newEdObj=(*i);
+		newEdObj.pos = i;//correcting pos
 
+		VertexObject& vObj = *(v.v_obj);
+		EdgeList & v_edges =    *(vObj.inc_edges_pos); //List of edges incident to the v
+		VertexObject& W = *(w.v_obj);
+		EdgeList & w_edges =	*(W.inc_edges_pos); //List of edges incident to the w
+
+		Edge newEdge; 
+		newEdge.e_obj=&newEdObj;//creating new edge for inserting
+		w_edges.push_back(newEdge);//adding new edge to the incdient list for v
+		v_edges.push_back(newEdge);//adding new edge to the incident list for w
+		newEdObj.origin_inc_edges_pos=--(v_edges.end());//Correcting the origin_incident list position
+		newEdObj.dest_inc_edges_pos=--(w_edges.end());//correcting the destination_ incident list position;
+		return newEdge;
+		
+	}	
+	else 
+		throw runtime_error("Already exists");
   }
-
   /*
    * Remove a vertex from this graph. All edges that contain
    * v as one of their vertices are also removed.
@@ -327,7 +439,12 @@ public:
    * v - a vertex
    */
   void eraseVertex(const Vertex& v) {
-
+	EdgeList& edgeList = *(*v.v_obj).inc_edges_pos;//list of edges which need to be deleted
+	  for(EdgeItor i = edgeList.begin(); i != edgeList.end();){//deleting associated edges
+		eraseEdge(*i);
+	}
+	vertex_collection.erase((*v.v_obj).pos);// erasing vertex from vertex_collection
+	delete &v;//deleting the vertex itse;f
   }
 
   /*
@@ -336,7 +453,11 @@ public:
    * e - an edge
    */
   void eraseEdge(const Edge& e) {
-
+	EdgeObject& edObj=*(e.e_obj);
+	(*(edObj.origin_vertex.v_obj->inc_edges_pos)).erase(edObj.origin_inc_edges_pos);//Deleting origin pos form the collection
+        (*(edObj.dest_vertex.v_obj->inc_edges_pos)).erase(edObj.dest_inc_edges_pos);//Deleting destination pos from the collection
+        edge_collection.erase(edObj.pos);//deleting edge from the collection
+        delete &e;//deleting edge e
   }
 
 };
--- assignment6/assignment6.cpp
+++ assignment6/assignment6.cpp
@@ -8,22 +8,104 @@
 #include <limits>
 #include "AdjacencyListDirectedGraph.h"
 #include "FlightMap.h"
-
+#include <unordered_map>
 
 double FlightMap::calcRouteDistance(const list<string> route) {
+	//We will check the three conditions( existence of the airports and the edge between them), then find an edge between aitports and add distance to the answer through loop
+	double answer=0;
+	for(list<string>::const_iterator iter = route.begin(); iter != route.end();)
+		{FlightGraph::Vertex v1, v2;
+		if (!findAirport(v1, *iter)) {
+  			  throw runtime_error(*iter + " does not exist.");
+		 }
+ 	        if (!findAirport(v2, *(++iter))) {
+   			 throw runtime_error(*(iter) + " does not exist.");
+ 		 }
+ 	        if (!v1.isOutgoingTo(v2)) {
+   			 throw runtime_error(*v1 + " does not connect to " + *v2 + ".");
 
-
-}
+   		 }
+		FlightGraph::Edge edge = v1.outgoingEdge(v2);
+		answer+=*(edge);
+	}
+	return answer;
+	}
 
 
 list<string> FlightMap::findShortestRoute(const string &airport1, const string &airport2) {
+	if(!isAirportExist(airport1))
+		throw runtime_error(airport1 + " does not exist.");
+	if(!isAirportExist(airport2))
+                throw runtime_error(airport2 + " does not exist.");
+	FlightGraph::Vertex src, dst;
+	FlightGraph::VertexList vlist = flight_graph.vertices();
+	src=airport_db.at(airport1);
+	dst=airport_db.at(airport2);
+	int a = std::numeric_limits<int>::max();
+	typedef std::pair<int, string> cur;//closest graph and distance
+	priority_queue<cur, vector<cur>, greater<cur>> unvisited;//unvisited
+	map<string, cur> D;
+	for (FlightGraph::VertexItor iter = vlist.begin(); iter != vlist.end(); iter) 
+        {
+		D[**iter].first=a;       
+        }
 
-
-}
-
+	unvisited.push(std::make_pair(0, *src));
+	D[*src].first=0;
+	bool check=true;
+	while(!unvisited.empty())
+	{
+		FlightGraph::Vertex tmp = airport_db.at(unvisited.top().second);
+		unvisited.pop();
+		FlightGraph::EdgeList elist = tmp.outgoingEdges();
+		for(FlightGraph::EdgeItor iter = elist.begin(); iter != elist.end(); ++iter)
+		{
+      				int weight =**iter;
+				 if (D[*(iter->dest())].first > D[*tmp].first + weight)
+           			 {
+					D[*(iter->dest())].first = D[*tmp].first + weight;
+					D[*(iter->dest())].second = *tmp;
+               				unvisited.push(make_pair(D[*(iter->dest())].first, *(iter->dest())));
+			   	 }
+				if(*(iter->dest())==*dst)
+				{
+					check=false;
+				}
+		
+					
+   		 }
+	}
+/*
+	list<string> ans;
+	string dest=*dst;
+	if(!check)
+	{	while(dest==(*src))
+			{	
+				ans.push_front(dest);
+				dest=D[dest].second;
+			}
+	}
+	ans.push_front(*src);
+	return ans;
+*/	
+  }
+	
+			
+	
 
 void FlightMap::printAllShortestRoutes(const string &airport) {
-
-
+	if(!isAirportExist(airport))
+                throw runtime_error(airport + " does not exist.");
+	FlightGraph::VertexList vlist = flight_graph.vertices();
+	for(FlightGraph::VertexItor iter = vlist.begin(); iter != vlist.end(); ++iter)
+	{	if(**iter!=airport)
+		{
+			list<string> route=findShortestRoute(airport, **iter);
+			if(route.size()>0)
+				{
+					 printRoute(route);
+					 cout << "  (Distance = " << calcRouteDistance(route) << ")" << endl;
+				}
+		}
+	}
 }
-
